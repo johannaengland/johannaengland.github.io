e46c50bca87fe953cd206b58b12136d896e16440
johannaengland <39082189+johannaengland@users.noreply.github.com>
2018-12-10 15:47:17 +0100

    Create publications.js

---
+++ js/publications.js
@@ -0,0 +1,27 @@
+<script>
+var currentSlide = 1;
+const numberOfSlides = 3;
+
+function slideshow(direction) {
+    if (direction === -1) {
+        if (currentSlide !== 1) {
+            currentSlide = currentSlide - 1;
+        } 
+        else {
+            currentSlide = numberOfSlides;
+        }
+    }
+    else {
+        if (currentSlide !== numberOfSlides) {
+            currentSlide = currentSlide + 1;
+        } 
+        else {
+            currentSlide = 1;
+        }
+    }
+    
+    document.getElementById('slidecount').value = currentSlide -1;
+    document.getElementById('slidecount').max = numberOfSlides -1;
+    document.getElementById('currentSlide').src = "images/pic"+currentSlide+".jpg";
+}
+</script>
