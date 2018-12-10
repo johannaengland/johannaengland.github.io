19d456a254eb90e633660aff9faef86e8e2d1f95
johannaengland <39082189+johannaengland@users.noreply.github.com>
2018-12-10 17:49:00 +0100

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -33,17 +33,17 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
     
 // When the user clicks the button, open the modal 
-let btn.onclick = function() {
+btn.onclick = function() {
     modal.style.display = "block";
 };
     
 // When the user clicks on <span> (x), close the modal
-let span.onclick = function() {
+span.onclick = function() {
     modal.style.display = "none";
 };
     
 // When the user clicks anywhere outside of the modal, close it
-let window.onclick = function(event) {
+window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
