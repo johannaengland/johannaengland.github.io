37d51e3d3d18d483932db80953c8b684cc172f5f
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-01-07 13:50:40 +0100

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -1,4 +1,4 @@
-var currentSlide = 1;
+let currentSlide = 1;
 const numberOfSlides = 3;
 
 function slideshow(direction) {
@@ -23,30 +23,9 @@ function slideshow(direction) {
     document.getElementById('slidecount').max = numberOfSlides -1;
     document.getElementById('currentSlide').src = "images/pic"+currentSlide+".jpg";
 }
-/*
-//Das ist alles für den Bibtex-Button
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-    
-// When the user clicks the button, open the modal 
-function showModal() {
-    modal.style.display = "block";
-};
-btn.onclick = showModal();
-    
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-};
-    
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-*/
+
+let bibtexText = document.getElementById("bibtexText");
+
+function showBibtexText () {
+    bibtexText.classList.toggle("bibtexTextHidden");
+}
