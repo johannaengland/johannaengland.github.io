d29aed84fbee8e719f9c0428ff270d4d515fa2ca
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-02-11 10:02:07 +0100

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -21,7 +21,7 @@ function slideshow(direction) {
     
     document.getElementById('slidecount').value = currentSlide -1;
     document.getElementById('slidecount').max = numberOfSlides -1;
-    document.getElementById('currentSlide').src = "images/pic"+currentSlide+".jpg";
+    document.getElementById('currentSlide').src = "/slides/"+currentSlide+".jpg";
 }
 
 function showBibtexText () {
