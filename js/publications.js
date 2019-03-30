4f752d29cb2b773dcc2eafa203377efcd7494f8c
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-03-30 16:19:07 +0100

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -29,17 +29,3 @@ function slideshow(direction) {
 function showBibtexText () {
     document.getElementById("bibtexText").classList.toggle("bibtexTextHidden");
 }
-
-window.onload = function checkDownloads() {
-    pdf = document.getElementById(pdf);
-    console.log(pdf);
-    pdf.href = "/downloads/publication.pdf";
-    pdf.onerror = function deletePdfElement() {
-        document.getElementById(tdpdf).removeChild(pdf);
-    }
-    pptx = document.getElementById(pptx)
-    pptx.href = "/downloads/slides.pptx";
-    pptx.onerror = function deletePptxElement() {
-        document.getElementById(tdptx).removeChild(pptx);
-    }
-};
