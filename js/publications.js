e5e4ca4c52aa3f814fa9ffd80fb9fa444e7dc847
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-03-30 14:56:11 +0100

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -1,24 +1,6 @@
 let testSlideNumber = 1;
 let numberOfTestSlides = -1;
-
-window.onload = countSlides;
-
-function countSlides() {
-    testSlide = document.getElementById('testSlide');
-    testSlide.style.display = "none";
-    while(numberOfTestSlides == -1) {
-        testSlideNumber++;
-        testSlide.onerror=setNumberOfTestSlides;
-        testSlide.src= "/slides/pic"+testSlideNumber+".jpg";
-        function setNumberOfTestSlides() {
-            numberOfTestSlides = testSlideNumber-1;
-        }
-    }
-    testSlideNumber = 1;
-    testSlide.src= "/slides/pic"+testSlideNumber+".jpg";
-    testSlide.style.display = "initial";
-}
-
+    
 function slideshow(direction) {
     testSlide = document.getElementById('testSlide');
     testSlideNumber += direction;
@@ -27,10 +9,12 @@ function slideshow(direction) {
 
     function errorHandling() {
         if(direction == -1) {
-            testSlideNumber = numberOfTestSlides;
+            if(numberOfTestSlides == -1) testSlideNumber = 1;
+            else testSlideNumber = numberOfTestSlides;
             testSlide.src = "/slides/pic"+testSlideNumber+".jpg";
         }
         else if (direction == 1) {
+            if(numberOfTestSlides == -1) numberOfTestSlides = testSlideNumber-1;
             testSlideNumber = 1;
             testSlide.src = "/slides/pic"+testSlideNumber+".jpg";
         }
