448784dc888e82a1af2ad9e99af9c44d95de829e
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-03-30 14:11:11 +0100

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -1,6 +1,8 @@
 let currentSlide = 1;
-const numberOfSlides = 3;
-
+let testSlideNumber = 1;
+let testSlideNumber = -1;
+let numberOfSlides = 3;
+    
 function slideshow(direction) {
     if (direction === -1) {
         if (currentSlide !== 1) {
@@ -18,12 +20,23 @@ function slideshow(direction) {
             currentSlide = 1;
         }
     }
-    
+        
     document.getElementById('slidecount').value = currentSlide -1;
     document.getElementById('slidecount').max = numberOfSlides -1;
     document.getElementById('currentSlide').src = "/slides/pic"+currentSlide+".jpg";
-}
 
+    testSlide = document.getElementById('testSlide');
+    testSlideNumber += direction;
+    tester.onerror=errorHandling;
+    tester.src= "/slides/pic"+testSlideNumber+".jpg";
+
+    function errorHandling() {
+        numberOfTestSlides = testSlideNumber-1;
+        testSlideNumber = 1;
+        tester.src = "/slides/pic"+testSlideNumber+".jpg";
+    }
+}
+    
 function showBibtexText () {
     document.getElementById("bibtexText").classList.toggle("bibtexTextHidden");
 }
