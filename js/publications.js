1a83b041e1dc364315fa561460620ca8e0b16df6
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-03-30 14:24:17 +0100

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -1,30 +1,7 @@
-let currentSlide = 1;
 let testSlideNumber = 1;
-let testSlideNumber = -1;
-let numberOfSlides = 3;
+let numberOfTestSlides = -1;
     
 function slideshow(direction) {
-    if (direction === -1) {
-        if (currentSlide !== 1) {
-            currentSlide = currentSlide - 1;
-        } 
-        else {
-            currentSlide = numberOfSlides;
-        }
-    }
-    else {
-        if (currentSlide !== numberOfSlides) {
-            currentSlide = currentSlide + 1;
-        } 
-        else {
-            currentSlide = 1;
-        }
-    }
-        
-    document.getElementById('slidecount').value = currentSlide -1;
-    document.getElementById('slidecount').max = numberOfSlides -1;
-    document.getElementById('currentSlide').src = "/slides/pic"+currentSlide+".jpg";
-
     testSlide = document.getElementById('testSlide');
     testSlideNumber += direction;
     tester.onerror=errorHandling;
