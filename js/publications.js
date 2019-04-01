322b76838e41dc99dcd0202f8ac013af19dd09f8
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-04-01 16:17:22 +0200

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -35,7 +35,7 @@ function showBibtexText () {
 window.onload = function atStart() {
     url = document.getElementById("url").innerHTML.substring(15);
     nameOfSlides = document.getElementById("nameOfSlides").innerHTML;
-    nameOfSlides = document.getElementById("fileEnding").innerHTML;
+    fileEnding = document.getElementById("fileEnding").innerHTML;
     console.log("/slides/"+url+"nameOfSlides: "+nameOfSlides+"slideNumber:"+slideNumber+"FE: "+fileEnding);
     //document.getElementById('currentSlide').src = "/slides/"+url+nameOfSlides+slideNumber+fileEnding;
 }
