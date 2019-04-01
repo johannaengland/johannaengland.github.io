1feb0dd12cf57fc6a71e5e5873b8fcff65f13e5e
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-04-01 16:27:07 +0200

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -8,7 +8,7 @@ function slideshow(direction) {
     currentSlide = document.getElementById('currentSlide');
     slideNumber += direction;
     currentSlide.onerror=errorHandling;
-    currentSlide.src= "/slides/"+url+nameOfSlides+slideNumber+fileEnding;
+    currentSlide.src= "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding;
 
     function errorHandling() {
         if(direction == -1) {
@@ -16,14 +16,14 @@ function slideshow(direction) {
                 slideshow(1);
             }
             else slideNumber = numberOfSlides;
-            currentSlide.src= "/slides/"+url+nameOfSlides+slideNumber+fileEnding;        
+            currentSlide.src= "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding;        
         }
         else if (direction == 1) {
             if(numberOfSlides == -1) {
                 numberOfSlides = slideNumber-1;
             }
             slideNumber = 1;
-            currentSlide.src= "/slides/"+url+nameOfSlides+slideNumber+fileEnding;        
+            currentSlide.src= "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding;        
         }
     }
 }
@@ -36,6 +36,6 @@ window.onload = function atStart() {
     url = document.getElementById("url").innerHTML.substring(15);
     nameOfSlides = document.getElementById("nameOfSlides").innerHTML;
     fileEnding = document.getElementById("fileEnding").innerHTML;
-    console.log("/slides/"+url+nameOfSlides+slideNumber+fileEnding);
+    console.log("/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding);
     //document.getElementById('currentSlide').src = "/slides/"+url+nameOfSlides+slideNumber+fileEnding;
 }
