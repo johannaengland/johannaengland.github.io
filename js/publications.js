d687d1b1c1257e7759bb826a45a8182c14f11560
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-04-01 16:34:31 +0200

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -3,12 +3,15 @@ let numberOfSlides = -1;
 let url;
 let nameOfSlides;
 let fileEnding;
+let source;
     
 function slideshow(direction) {
     currentSlide = document.getElementById('currentSlide');
     slideNumber += direction;
     currentSlide.onerror=errorHandling;
-    currentSlide.src= "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding;
+    source = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
+    source = source.replace(/\s+/g, '');
+    currentSlide.src= source
 
     function errorHandling() {
         if(direction == -1) {
@@ -16,14 +19,18 @@ function slideshow(direction) {
                 slideshow(1);
             }
             else slideNumber = numberOfSlides;
-            currentSlide.src= "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding;        
+            source = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
+            source = source.replace(/\s+/g, '');
+            currentSlide.src= source;        
         }
         else if (direction == 1) {
             if(numberOfSlides == -1) {
                 numberOfSlides = slideNumber-1;
             }
             slideNumber = 1;
-            currentSlide.src= "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding;        
+            source = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
+            source = source.replace(/\s+/g, '');
+            currentSlide.src= source;        
         }
     }
 }
@@ -36,6 +43,8 @@ window.onload = function atStart() {
     url = document.getElementById("url").innerHTML.substring(15);
     nameOfSlides = document.getElementById("nameOfSlides").innerHTML;
     fileEnding = document.getElementById("fileEnding").innerHTML;
-    console.log("/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding);
+    source = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
+    source = source.replace(/\s+/g, '');
+    console.log(source);
     //document.getElementById('currentSlide').src = "/slides/"+url+nameOfSlides+slideNumber+fileEnding;
 }
