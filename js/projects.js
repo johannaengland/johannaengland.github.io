1e00d8aa102f2e4459a423967c2f36b9cd16d813
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-04-07 19:23:52 +0200

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,14 +1,11 @@
 window.onload = function atStart() {
     let publications = document.getElementsByClassName("publications");
-    console.log(publications);
     for(let i=0;i<publications.length;i++) {
         let name = publications[i].innerHTML;
         publications[i].innerHTML = "";
         let link = document.createElement("a");
         link.innerHTML = name;
         link.href= "/publications/"+name;
-        console.log(link);
-        console.log(publications[i]);
         publications[i].appendChild(link);
     }
 }
