bbc66b3298350f83eb610fdfd0d8913d1583cef6
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-04-15 15:28:06 +0200

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -3,15 +3,18 @@ let numberOfSlides = -1;
 let url;
 let nameOfSlides;
 let fileEnding;
-let source;
+let sourceSlides;
+let bibtex;
+let powerpoint;
+let pdf;
     
 function slideshow(direction) {
     currentSlide = document.getElementById('currentSlide');
     slideNumber += direction;
     currentSlide.onerror=errorHandling;
-    source = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
-    source = source.replace(/\s+/g, '');
-    currentSlide.src= source
+    sourceSlides = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
+    sourceSlides = sourceSlides.replace(/\s+/g, '');
+    currentSlide.src= sourceSlides;
 
     function errorHandling() {
         if(direction == -1) {
@@ -19,31 +22,42 @@ function slideshow(direction) {
                 slideshow(1);
             }
             else slideNumber = numberOfSlides;
-            source = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
-            source = source.replace(/\s+/g, '');
-            currentSlide.src= source;        
+            sourceSlides = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
+            sourceSlides = sourceSlides.replace(/\s+/g, '');
+            currentSlide.src= sourceSlides;        
         }
         else if (direction == 1) {
             if(numberOfSlides == -1) {
                 numberOfSlides = slideNumber-1;
             }
             slideNumber = 1;
-            source = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
-            source = source.replace(/\s+/g, '');
-            currentSlide.src= source;        
+            sourceSlides = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
+            sourceSlides = sourceSlides.replace(/\s+/g, '');
+            currentSlide.src= sourceSlides;        
         }
     }
 }
 
-function showBibtexText () {
-    document.getElementById("bibtexText").classList.toggle("bibtexTextHidden");
+function downloadSources() {
+    pdf = document.getElementsByClassName()[0];
+    powerpoint = document.getElementsByClassName()[1];
+    bibtex = document.getElementsByClassName()[2];
+
+    console.log(pdf);
+    console.log(("/downloads/"+url+"/publication.pdf").replace(/\s+/g, ''));
+
+    //pdf.href = ("/downloads/"+url+"/publication.pdf").replace(/\s+/g, '');
+    //powerpoint.href = ("/downloads/"+url+"/slides.pptx").replace(/\s+/g, '');
+    //bibtex.href = ("/downloads/"+url+"/bibtex.bib").replace(/\s+/g, '');
 }
 
 window.onload = function atStart() {
     url = document.getElementById("url").innerHTML.substring(15);
     nameOfSlides = document.getElementById("nameOfSlides").innerHTML;
     fileEnding = document.getElementById("fileEnding").innerHTML;
-    source = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
-    source = source.replace(/\s+/g, '');
-    document.getElementById('currentSlide').src = source;
+    sourceSlides = "/slides/"+url+"/"+nameOfSlides+slideNumber+fileEnding; 
+    sourceSlides = sourceSlides.replace(/\s+/g, '');
+    document.getElementById('currentSlide').src = sourceSlides;
+
+    downloadSources();
 }
