55fe1fcee0c928ff38af0d661276d3aa42d26d47
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-04-30 09:54:42 +0200

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -4,7 +4,14 @@ let url;
 let nameOfSlides;
 let fileEnding;
 let source;
-    
+
+/*This functions controls the slideshow (if you press the previous button the direction will be -1 and the next will be 1)
+//If an error occurs (you want to show a picture, that doesn't exist), you either start at the first picture 
+//(if you pressed next on the last slide or previous on the first slide and don't know yet how many slides there are,
+//you will know after going through all of them by pressing next and then starting at the first again)
+//or if you already know the number of slides and press previous on the first slide it will go to the first slide
+//This is not optimal, but I didn't find a way to know the total number of slides before the user went through all of them
+*/
 function slideshow(direction) {
     currentSlide = document.getElementById('currentSlide');
     slideNumber += direction;
@@ -35,6 +42,7 @@ function slideshow(direction) {
     }
 }
 
+//this function will add the download links from knowing the name of the publication file and then navigating inside of the downloads-folder
 function downloadSources() {
     pdf = document.getElementsByClassName('downloads')[0];
     powerpoint = document.getElementsByClassName('downloads')[1];
@@ -44,6 +52,7 @@ function downloadSources() {
     bibtex.href = ("/downloads/"+url+"/bibtex.bib").replace(/\s+/g, '');
 }
 
+//This function prepares for the slideshow function
 window.onload = function atStart() {
     url = document.getElementById("url").innerHTML.substring(15);
     nameOfSlides = document.getElementById("nameOfSlides").innerHTML;
