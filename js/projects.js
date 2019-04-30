6a8478a7d8474abf4d665a7f82d0ed06e80aaafd
johannaengland <39082189+johannaengland@users.noreply.github.com>
2019-04-30 09:44:34 +0200

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,5 @@
-window.onload = function atStart() {
+//This function takes the names of the Publications related to a project and creates links to the pages of these
+window.onload = function addLinks() {
     let publications = document.getElementsByClassName("publications");
     for(let i=0;i<publications.length;i++) {
         let name = publications[i].innerHTML;
